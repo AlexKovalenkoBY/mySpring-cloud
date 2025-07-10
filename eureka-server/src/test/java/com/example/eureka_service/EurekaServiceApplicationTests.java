17e5a4b4fbe27a5cac85addf773c80eabcddb96c
AlexKovalenkoBY <kovalenkoam@gmail.com>
2025-07-10 14:09:23 +0300

    add new-service

---
+++ eureka-server/src/test/java/com/example/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_service; // Исправлен package (должен совпадать с основным классом)
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Тест проверяет, что контекст Spring загружается корректно
+    }
+}
---
+++ new-service/src/test/java/com/example/new_service/NewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
